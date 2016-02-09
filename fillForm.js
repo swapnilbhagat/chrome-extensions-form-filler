6b5e5dc35c8d0545340d05d5fc82b8394d06ce84
Swapnil Bhagat <swapnil.bhagat@talentica.com>
2016-02-10 00:16:58 +0530

    updated

--- fillForm.js
+++ fillForm.js
@@ -1,12 +1,13 @@
 
 function fillForm() {
-	alert('Fill Form');
 	
 	window.document.getElementById("firstName").value="Swapnil";
 	window.document.getElementById("lastName").value="Bhagat";
 	window.document.getElementById("city").value="Pune";
 	
 	window.document.getElementById("text").innerHTML = 'Text that got changed';
+	
+	window.alert("Filled Form");
 }
 
 chrome.runtime.sendMessage({
